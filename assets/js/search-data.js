656c287d049b93954a1f97631d4d019eb403a747
xwpken <58301736+xwpken@users.noreply.github.com>
2026-04-12 15:46:25 +0000

    Deploying to gh-pages from @ xwpken/xwpken.github.io@cab7fd96ecd872d63678257035f61b69763fda55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
